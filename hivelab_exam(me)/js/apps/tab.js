d52335f92ca923d025af8872304b513a2e2398de
hvl1099 <hvl1099@gepplus.co.kr>
2017-10-20 14:03:24 +0900

    hivelab_ex

---
+++ hivelab_exam(me)/js/apps/tab.js
@@ -0,0 +1,11 @@
+(function (win, $) {
+    'use strict';
+    win.projectName = win.projectName || {};
+    win.projectName.common = win.projectName.common || {};
+
+    var UTIL = win.projectName.common.util;
+
+    $(function () {
+        win.console.log(UTIL);
+    });
+})(window, window.jQuery);
