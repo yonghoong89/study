a665fd429ea2284b34e015dd7985303b280a4ece
yonghoong <yonghoong89@naver.com>
2019-01-18 09:22:58 +0900

    react add

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movietitle = ["제목1","제목2","제목3"]
+const movieimg = ["111","222","333"]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
-        <Movie />
-        <Movie />
+        <Movie title={movietitle[0]} poster={movieimg[0]} />
+        <Movie title={movietitle[1]} poster={movieimg[1]} />
+        <Movie title={movietitle[2]} poster={movieimg[2]} />
       </div>
     );
   }
--- react/my-app/src/Movie.js
+++ react/my-app/src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component {
     render() {
       return (
         <div>
-          <h1>영화제목</h1>
-          <Movieposter/>
+          <h1>{this.props.title}</h1>
+          <Movieposter poster={this.props.poster}  />
         </div>
       )
     }
@@ -15,7 +15,7 @@ class Movie extends Component {
 class Movieposter extends Component {
   render() {
     return (
-      <img src="https://search.pstatic.net/common?type=u&size=100x133&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20181227_80%2F1545901137289EGbWK_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2"/>
+      <img src={this.props.poster} />
     )
   }
 }
