0e6356a0e5d2dde93dd74d9b30929a5224e1352e
yonghoong <yonghoong89@naver.com>
2019-02-18 15:27:02 +0900

    react add test

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -2,40 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {title:"제목1",poster:"111"},{title:"제목2",poster:"222"},{title:"제목3",poster:"333"}
-]
-
 class App extends Component {
 
-  state = {
-
-  }
+  state = {}
 
   componentDidMount(){
-    setTimeout( ()=> {
-      this.setState({
-        movies : [
-          {title:"제목1",poster:"111"},{title:"제목2",poster:"222"},{title:"제목3",poster:"333"},{title:"add",poster:"444"}
-        ]
-      })
-    }, 1000)
+    this._getMoives()
   }
 
   _renderMovies = () =>{
-    const movies = this.state.movies.map((movie,index)=>{
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie)=>{
+      return <Movie 
+      title={movie.title}
+      poster={movie.medium_cover_image} 
+      key={movie.id}
+      genres={movie.genres} 
+      synopsis={movie.synopsis} />
     });
     return movies
   }
 
+  _getMoives = async () =>{
+    const movies = await this._callapi() 
+    this.setState({
+      movies:movies //movies 모던 자바스크립트
+    })
+  }
+
+  _callapi = () =>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json())//데이터를 상황에 맞게 편집 할 수 있음
+    .then(json => json.data.movies)
+    .catch(error => console.log(error))
+  }
+
   render() {
+    const {movies} = this.state; 
     return (
-      <div className="App">
+      <div className={movies ? "app" : "app_loading"}>
        {this.state.movies ? this._renderMovies() : "loading"}
       </div>
     );
   }
+  
 }
 
 export default App;
--- react/my-app/src/Movie.js
+++ react/my-app/src/Movie.js
@@ -1,30 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import LinesEllipsis from 'react-lines-ellipsis'
 
-
-class Movie extends Component {
-
-  // static PropTypes = {
-  //   title:PropTypes.string.isRequried,
-  //   poster:PropTypes.string
-  // }
-
-  render() {
-      return (
-        <div>
-          <h1>{this.props.title}</h1>
-          <Movieposter poster={this.props.poster}  />
+function Movie({title,poster,genres,synopsis}){
+  return(
+    <div className="movie">
+      <div className="movie_column">
+        <Movieposter poster={poster} alt={title} />
+      </div>
+      <div className="movie_column">
+        <h1>{title}</h1>
+        <div className="movie_genres">
+          {genres.map((genre,index)=><Moviegenre genre={genre} key={index} />)}
         </div>
-      )
-    }
+        <div className="movie_synopsis">           
+          <LinesEllipsis
+            text= {synopsis}
+            maxLine='3'
+            ellipsis='...'
+            trimRight
+            basedOn='letters'
+          />
+        </div>    
+      </div>
+    </div>
+  )
 }
 
-class Movieposter extends Component {
-  render() {
-    return (
-      <img src={this.props.poster} />
-    )
-  }
+function Movieposter({poster,alt}){
+  return(
+    <img src={poster} title={alt} alt={alt} className="movie_poster" />
+  )
 }
+function Moviegenre({genre}){
+  return(
+    <span className="movie_genre">{genre}</span>
+  )
+}
+
+Movie.propTypes ={
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
+}
+
+Movieposter.propTypes ={
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+}
+
+Moviegenre.propTypes ={
+  genre: PropTypes.string.isRequired
+}
+
 export default Movie
